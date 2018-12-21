ec5253df71dac0780cc6ba6bcdc7920c22491763
satheeshdragon.satheesh@gmail.com <satheeshdragon.satheesh@gmail.com>
2018-12-21 15:36:28 +0530

    2018-12-21

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a comment!
 echo Hello World	# This is a comment, too!
+git remote add origin https://github.com/satheeshdragon/panel_pro.git
 git config --global user.name "satheeshdragon.satheesh@gmail.com"
 git checkout
 git add .
-git remote add origin https://github.com/satheeshdragon/panel_pro.git
 DATE=`date +%Y-%m-%d`
 git commit -m "$DATE"
 git push origin master
